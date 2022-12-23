f7ab2671a228968c85fb446d7b2fe71c7284f583
Razan Aljohani <47436737+razanmaljuhani@users.noreply.github.com>
2022-12-23 20:06:41 +0300

    Delete BAR_1806065_P5_BookBankSystem/BAR_1806065_P5_BookBankSystem directory

--- BAR_1806065_P5_BookBankSystem/BAR_1806065_P5_BookBankSystem/src/bar_1806065_p5_bookbanksystem/MaterialNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package bar_1806065_p5_bookbanksystem;
-
-public class MaterialNotFoundException extends Exception {
-
-    public MaterialNotFoundException(String title) {
-        super("Error: The material titled " + title + " was not found in this Book Bank .");
-    }
-}
-
